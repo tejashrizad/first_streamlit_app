2bda4e2393b334bf0ae7a0d0ed417812460e079c
tejashrizad <33203298+tejashrizad@users.noreply.github.com>
2023-09-06 22:51:31 +0530

    Update streamlit_app.py
    corrected indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,8 +70,8 @@ if streamlit.button('Get Fruit Load List'):
 #Allow end user to add fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-  my_cur.execute("insert into fruit_load_list values('from streamlit')")
-  return "Thanks for adding" + add_my_fruit
+    my_cur.execute("insert into fruit_load_list values('from streamlit')")
+    return "Thanks for adding" + add_my_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
